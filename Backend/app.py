cb958acf88cd8af4ef7c526e6110509bdb2a6452
Prishu-04 <pratyakshsrivastava23ks@gmail.com>
2026-06-30 13:19:37 +0530

    Remove old compiled Python files and update app entry point for better execution

--- Backend/app.py
+++ Backend/app.py
@@ -254,4 +254,10 @@ def api_charts():
 
 
 
-if __name__ == " __main__\:
+
+
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
